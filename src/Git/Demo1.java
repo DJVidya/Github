fe9f9f1c6ce1cae4af0c220c52e59f8cb9e74042
evidydj <evidydj@5CD6093P6N.ericsson.se>
2019-05-14 08:09:31 +0530

    second time

--- src/Git/Demo1.java
+++ src/Git/Demo1.java
@@ -3,8 +3,9 @@ package Git;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("first time");
+		System.out.println("mxncvjnkcvnjnvknc");
+		
 	}
 
 }
